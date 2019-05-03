17b26c945d75121edff853ef9a419fe0ff5c319c
Tiffany B <tjbrincefield@gmail.com>
2019-05-02 20:31:59 -0400

    added more info on the js file and some stylings files installed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,110 +1,70 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var colors = require('colors');
+// const cTable = require('console.table');;
+
+colors.setTheme({
+  silly: 'rainbow',
+  input: 'grey',
+  verbose: 'cyan',
+  prompt: 'grey',
+  info: 'green',
+  data: 'grey',
+  help: 'cyan',
+  warn: 'yellow',
+  debug: 'blue',
+  error: 'red'
+});
 
 var connection = mysql.createConnection({
   host: "localhost",
-
   port: 3306,
-
   user: "root",
-
   password: "Spring21",
   database: "bamazonDB"
 });
 
 connection.connect(function(err) {
   if (err) throw err;
-  start();
+  chooseItem();
 });
 
+
+// function displayItems() {
+//   var table = mysql -uroot products --table < schema.sql;
+// }
+
+function chooseItem() {
+  connection.query("SELECT * FROM products", function(err, results) {
+      if (err) throw err;
+      // once you have the items, prompt the user for which they'd like to bid on
+      inquirer.prompt([
+                    {
+                      type: 'input',
+                      name: 'item',
+                      message: 'Please enter the Item ID that you would like to purchase.',
+                      filter: Number
+                    },
+                  ]).then(start) 
+                    // switch (answer.input) {
+                    //   case "How many do you need?":
+                    //     start();
+                    //     break;
+                      
+                    // })
+  })
+  };
+              
 function start() {
   inquirer.prompt([
 		{
 			type: 'input',
-			name: 'item',
-      message: 'Please enter the Item ID that you would like to purchase.',
-			filter: Number
-		},
-		{
-			type: 'input',
-			name: 'quantity',
-      message: 'How many do you need?',
+			name: 'Quantity',
+      // message: 'How many do you need?',
 			filter: Number
 		}
     ]).then(function(input) {
-      console.log("You have selected:" + input.item + input.quantity)
-        // based on their answer, either call the bid or the post functions
-        // if (answer.postOrBid === "POST") {
-        //   postAuction();
-        // }
-        // else if(answer.postOrBid === "BID") {
-        //   bidAuction();
-        // } else{
-        //   connection.end();
-        // }
-      });
-    };
-  
-  // function bidAuction() {
-  //   // query the database for all items being auctioned
-  //   connection.query("SELECT * FROM auctions", function(err, results) {
-  //     if (err) throw err;
-  //     // once you have the items, prompt the user for which they'd like to bid on
-  //     inquirer
-  //       .prompt([
-  //         {
-  //           name: "choice",
-  //           type: "rawlist",
-  //           choices: function() {
-  //             var choiceArray = [];
-  //             for (var i = 0; i < results.length; i++) {
-  //               choiceArray.push(results[i].item_name);
-  //             }
-  //             return choiceArray;
-  //           },
-  //           message: "What auction would you like to place a bid in?"
-  //         },
-  //         {
-  //           name: "bid",
-  //           type: "input",
-  //           message: "How much would you like to bid?"
-  //         }
-  //       ])
-  //       .then(function(answer) {
-  //         // get the information of the chosen item
-  //         var chosenItem;
-  //         for (var i = 0; i < results.length; i++) {
-  //           if (results[i].item_name === answer.choice) {
-  //             chosenItem = results[i];
-  //           }
-  //         }
-  
-  //         // determine if bid was high enough
-  //         if (chosenItem.highest_bid < parseInt(answer.bid)) {
-  //           // bid was high enough, so update db, let the user know, and start over
-  //           connection.query(
-  //             "UPDATE auctions SET ? WHERE ?",
-  //             [
-  //               {
-  //                 highest_bid: answer.bid
-  //               },
-  //               {
-  //                 id: chosenItem.id
-  //               }
-  //             ],
-  //             function(error) {
-  //               if (error) throw err;
-  //               console.log("Bid placed successfully!");
-  //               start();
-  //             }
-  //           );
-  //         }
-  //         else {
-  //           // bid wasn't high enough, so apologize and start over
-  //           console.log("Your bid was too low. Try again...");
-  //           start();
-  //         }
-  //       });
-  //   });
-  // }
-  
+      console.log("You have selected: \n Item " + input.item + '\n Quantity ' + input.Quantity)
+        });
+      };
+      
